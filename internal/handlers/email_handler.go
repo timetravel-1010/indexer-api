4b08ae3212d3f169996c98c6a703300fe2aeaafa
timetravel-1010 <timetravel_1010@protonmail.com>
2024-01-30 22:09:19 -0500

    Add Zinc Package to Manage Requests and Queries

--- internal/handlers/email_handler.go
+++ internal/handlers/email_handler.go
@@ -2,61 +2,20 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
-	"github.com/timetravel-1010/indexer-api/internal/models"
+	"github.com/timetravel-1010/indexer-api/internal/zinc"
 )
 
 type EmailHandler struct{}
 
-type ZincResponse struct {
-	Hits struct {
-		Hits []models.Hit `json:"hits"`
-	} `json:"hits"`
-}
-
-type Config struct {
-	host     string
-	port     string
-	username string
-	password string
-}
-
-type ZincQuery struct {
-	query      []byte
-	params     map[string][]string
-	searchType ZincSearchType
-}
-
-type ZincSearchType string
-
-const (
-	match    ZincSearchType = "match"
-	matchall ZincSearchType = "matchall"
-
-	DEFAULT_PAGE_SIZE = 10
-)
-
 var (
-	requiredParams = []string{
-		"page",
-	}
-
-	matchQueryParams = append([]string{
-		"term",
-	}, requiredParams...)
-
-	matchAllQueryParams = append([]string{}, requiredParams...)
-
-	c Config = Config{
-		host:     "localhost",
-		port:     "4080",
-		username: "admin",
-		password: "Complexpass#123",
+	c = zinc.Config{
+		Host:     "localhost",
+		Port:     "4080",
+		Username: "admin",
+		Password: "Complexpass#123",
 	}
 )
 
@@ -74,15 +33,13 @@ func (eh EmailHandler) SearchByTerm(w http.ResponseWriter, r *http.Request) {
     "_source": [ ]
 }`)
 	//term := r.URL.Query().Get("term")
-	query, err := buildQuery(ZincQuery{
-		query:      queryTemp,
-		params:     r.URL.Query(),
-		searchType: match,
+	query, err := zinc.BuildQuery(zinc.ZincQuery{
+		Query:      queryTemp,
+		Params:     r.URL.Query(),
+		SearchType: zinc.MATCH_QUERY,
 	})
 
-	fmt.Println("url:", query)
-
-	res, err := doZincRequest(r, query)
+	res, err := zinc.DoZincRequest(r, query, c)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Fatal(err)
@@ -100,106 +57,16 @@ func (eh EmailHandler) GetEmails(w http.ResponseWriter, r *http.Request) {
 	    "max_results": %v,
 	    "_source": []
 	}`)
-	q, err := buildQuery(ZincQuery{
-		query:      query,
-		params:     r.URL.Query(),
-		searchType: matchall,
+	q, err := zinc.BuildQuery(zinc.ZincQuery{
+		Query:      query,
+		Params:     r.URL.Query(),
+		SearchType: zinc.MATCHALL_QUERY,
 	})
-	res, err := doZincRequest(r, q)
+	res, err := zinc.DoZincRequest(r, q, c)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Fatal(err)
 	}
 
 	json.NewEncoder(w).Encode(res)
-}
-
-func buildQuery(zq ZincQuery) (q string, err error) {
-	/*
-		if validPage() != nil {
-			numResults := DEFAULT_PAGE_SIZE
-			n, err := strconv.Atoi(page)
-			if err != nil {
-				return "", err
-			}
-			numResults = n
-
-		}
-	*/
-	switch zq.searchType {
-	case match:
-		return buildMatchQuery(zq.query, zq.params)
-	case matchall:
-		return buildMatchAllQuery(zq.query, zq.params)
-	}
-
-	return "", nil
-}
-
-func getParams(params map[string][]string, toGet []string) (p []any, err error) {
-	for _, tg := range toGet {
-		urlParam := params[tg][0]
-		if urlParam == "" {
-			return nil, errors.New(fmt.Sprintf("missing param %s", p))
-		}
-		p = append(p, urlParam)
-	}
-	return p, nil
-}
-
-func buildMatchQuery(queryTemp []byte, params map[string][]string) (q string, err error) {
-	urlParams, err := getParams(params, matchQueryParams)
-	if err != nil {
-		return "", err
-	}
-	q = fmt.Sprintf(string(queryTemp), urlParams...)
-	return q, err
-}
-
-func buildMatchAllQuery(queryTemp []byte, params map[string][]string) (q string, err error) {
-	urlParams, err := getParams(params, matchAllQueryParams)
-	if err != nil {
-		return "", err
-	}
-	q = fmt.Sprintf(string(queryTemp), urlParams...)
-	return q, nil
-}
-
-func doZincRequest(r *http.Request, query string) (*ZincResponse, error) {
-	log.Println("url query:", r.URL.Query())
-
-	index := r.URL.Query().Get("index")
-	url := fmt.Sprintf("http://%s:%s/api/%s/_search", c.host, c.port, index)
-	req, err := http.NewRequest(
-		"POST",
-		url,
-		strings.NewReader(query),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	req.SetBasicAuth(c.username, c.password)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set(
-		"User-Agent",
-		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
-	)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Println(resp.StatusCode)
-
-	zr := &ZincResponse{}
-	err = json.NewDecoder(resp.Body).Decode(&zr)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-
-	return zr, nil
 }
---
+++ internal/zinc/zinc.go
@@ -0,0 +1,66 @@
+package zinc
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/timetravel-1010/indexer-api/internal/models"
+)
+
+// Config
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+}
+
+// ZincResponse
+type ZincResponse struct {
+	Hits struct {
+		Hits []models.Hit `json:"hits"`
+	} `json:"hits"`
+}
+
+// DoZincRequest
+func DoZincRequest(r *http.Request, query string, c Config) (*ZincResponse, error) {
+	log.Println("url query:", r.URL.Query())
+
+	index := r.URL.Query().Get("index")
+	url := fmt.Sprintf("http://%s:%s/api/%s/_search", c.Host, c.Port, index)
+	req, err := http.NewRequest(
+		"POST",
+		url,
+		strings.NewReader(query),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth(c.Username, c.Password)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set(
+		"User-Agent",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+	)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println(resp.StatusCode)
+
+	zr := &ZincResponse{}
+	err = json.NewDecoder(resp.Body).Decode(&zr)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	return zr, nil
+}
